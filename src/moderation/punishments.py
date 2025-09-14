9b95f885e7671689fcb7f2c7467dc018b8a84874
mealet <jasonragonter@gmail.com>
2025-09-14 18:22:43 +0300

    added kick command

--- src/moderation/punishments.py
+++ src/moderation/punishments.py
@@ -9,7 +9,17 @@ from loguru import logger
 
 """
 Раздел комманд для наказаний со стороны модерации.
+Пометка: user команды находятся в контекстном меню пользователя в пункте "Приложения".
 
+User Команды:
+    - Ban
+      |-- Открывает модальное окно для бана пользователя.
+      |-- После успешного бана высылает общедоступное сообщение с информацией.
+      |-- Бан безвременный, снятие происходит вручную.
+
+    - Kick
+      |-- Открывает модальное окно для кика пользователя.
+      |-- После успешного кика высылает общедоступное сообщение с информацией.
 """
 
 
@@ -31,6 +41,25 @@ class Punishments(commands.Cog):
         if user.bot:
             await interaction.response.send_message(
                 "❌ Нельзя забанить бота!", ephemeral=True
+            )
+            return
+
+        await interaction.response.send_modal(BanModal(user))
+
+    @nextcord.user_command(name="Kick", guild_ids=config.GUILD_IDS)
+    @filters.has_any_role([config.MODERATION_ROLES])
+    async def kick_application(
+        self, interaction: nextcord.Interaction, user: nextcord.Member
+    ):
+        if user.id == interaction.user.id:
+            await interaction.response.send_message(
+                "❌ Нельзя выгнать самого себя!", ephemeral=True
+            )
+            return
+
+        if user.bot:
+            await interaction.response.send_message(
+                "❌ Нельзя выгнать бота!", ephemeral=True
             )
             return
 
@@ -98,6 +127,64 @@ class BanModal(nextcord.ui.Modal):
 
             logger.info(
                 f"`{self.target.name} ({self.target.id}) was banned by `{interaction.user.name} ({interaction.user.id})`"
+            )
+        except Exception as exception:
+            await interaction.response.send_message(
+                f"❌ Произошла ошибка:\n```{exception}```", ephemeral=True
+            )
+
+
+class KickModal(nextcord.ui.Modal):
+    def __init__(self, target: nextcord.Member):
+        super().__init__(
+            title=f"Кик `{target.name} ({target.nick})`",
+            custom_id="moderation_kick_modal",
+        )
+
+        self.target = target
+
+        self.reason = nextcord.ui.TextInput(
+            label="Введите причину кика",
+            custom_id="reason",
+            placeholder="Нарушение правил чата",
+            style=nextcord.TextInputStyle.short,
+            required=True,
+        )
+
+        self.add_item(self.reason)
+
+    async def callback(self, interaction):
+        embed_info = nextcord.Embed(
+            colour=nextcord.Colour.red(), timestamp=datetime.datetime.now()
+        )
+
+        embed_info.add_field(
+            name="Пользователь выгнан с сервера",
+            value=f"Пользователь: {self.target.mention}\nПричина: {self.reason.value}",
+        )
+
+        # Проверка на наличие аватарки (если нет - заменяем на свою)
+        author_avatar_url = (
+            "https://cdn.icon-icons.com/icons2/2108/PNG/512/discord_icon_130958.png"
+            if interaction.user.avatar is None
+            else interaction.user.avatar.url
+        )
+
+        banned_avatar_url = (
+            "https://cdn.icon-icons.com/icons2/2108/PNG/512/discord_icon_130958.png"
+            if self.target.avatar is None
+            else self.target.avatar.url
+        )
+
+        embed_info.set_author(name=interaction.user.name, icon_url=author_avatar_url)
+        embed_info.set_thumbnail(url=banned_avatar_url)
+
+        try:
+            await self.target.ban(reason=self.reason.value)
+            await interaction.response.send_message(embed=embed_info)
+
+            logger.info(
+                f"`{self.target.name} ({self.target.id}) was kicked from server by `{interaction.user.name} ({interaction.user.id})`"
             )
         except Exception as exception:
             await interaction.response.send_message(
