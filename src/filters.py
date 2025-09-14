6a380854a468decf2e3c809281e1bf94aa416976
mealet <110933288+mealet@users.noreply.github.com>
2025-09-14 21:32:21 +0300

    Update filters.py

--- src/filters.py
+++ src/filters.py
@@ -10,7 +10,7 @@ from nextcord.ext import commands
 """
 
 
-def has_any_role(role_ids):
+def has_any_role(role_ids: list[int]):
     async def predicate(ctx):
         return any(role.id in role_ids for role in ctx.author.roles)
 
